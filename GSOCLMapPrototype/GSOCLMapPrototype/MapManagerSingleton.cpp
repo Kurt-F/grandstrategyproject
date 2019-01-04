3962d944674f4de3e57517ef012f863951c9b7bf
Thinker222 <es0197@yahoo.com>
2019-01-03 20:05:27 -0500

    Added tests

--- GSOCLMapPrototype/GSOCLMapPrototype/MapManagerSingleton.cpp
+++ GSOCLMapPrototype/GSOCLMapPrototype/MapManagerSingleton.cpp
@@ -7,7 +7,6 @@ MapManagerSingleton::MapManagerSingleton()
 {
 	this->map = new Map_Node[MAX_NUMBER_OF_NODES];
 	this->number_of_nodes = 0;
-	instance = this; 
 }
 
 MapManagerSingleton::~MapManagerSingleton()
@@ -15,13 +14,13 @@ MapManagerSingleton::~MapManagerSingleton()
 	delete(this->map); // I doubt the game will ever run without this object but who knows
 }
 
-MapManagerSingleton MapManagerSingleton::Get_Instance()
+MapManagerSingleton* MapManagerSingleton::Get_Instance()
 {
 	if (instance == nullptr)
 	{
 		instance = new MapManagerSingleton();
 	}
-	return *instance;
+	return instance;
 }
 
 bool MapManagerSingleton::Create_Connection(Map_Node &a, Map_Node &b, int freight_cost_per_lb, int travel_cost)
--- GSOCLMapPrototype/GSOCLMapPrototype/MapManagerSingleton.h
+++ GSOCLMapPrototype/GSOCLMapPrototype/MapManagerSingleton.h
@@ -9,7 +9,7 @@ private:
 	MapManagerSingleton();
 	~MapManagerSingleton();
 public: 
-	MapManagerSingleton Get_Instance();
+	static MapManagerSingleton* Get_Instance();
 	bool Create_Connection(Map_Node &a, Map_Node &b, int freight_cost_per_lb = 0, int travel_cost = 0);
 	bool Remove_Connection(Map_Node &a, Map_Node &b);
 	bool Add_Node(Map_Node m);
