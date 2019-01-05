62378693e7ac6459c6cdcdd4f8d8b63a3ec4b39d
Thinker222 <es0197@yahoo.com>
2019-01-04 21:25:47 -0500

    More gui stuff

--- GSOCLMapPrototype/MapGUI/MainWindow.xaml.cs
+++ GSOCLMapPrototype/MapGUI/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace MapGUI
 {
     /// <summary>
@@ -20,9 +21,45 @@ namespace MapGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        MapGUI.ViewModels.MainViewModel _view_model;
         public MainWindow()
         {
             InitializeComponent();
+            _view_model = new ViewModels.MainViewModel(Map);
         }
+
+        private void Plus_Btn(object sender, RoutedEventArgs e)
+        {
+            _view_model.AddRow_Column(1);
+        }
+
+        private void Minus_Btn(object sender, RoutedEventArgs e)
+        {
+            _view_model.AddRow_Column(-1);
+        }
+
+        private void Up_Btn(object sender, RoutedEventArgs e)
+        {
+            _view_model.MoveGridUpDown(-1);
+        }
+
+
+        private void Down_Btn(object sender, RoutedEventArgs e)
+        {
+            _view_model.MoveGridUpDown(1); 
+        }
+
+
+        private void Left_Btn(object sender, RoutedEventArgs e)
+        {
+            _view_model.MoveGridLeftRight(-1);
+        }
+
+
+        private void Right_Btn(object sender, RoutedEventArgs e)
+        {
+            _view_model.MoveGridLeftRight(1);
+        }
+
     }
 }
---
+++ GSOCLMapPrototype/MapGUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MapGUI.ViewModels
+{
+    public class MainViewModel
+    {
+        #region private variables
+        const int MAX_COL_ROWS = 100;
+        int num_col_row; // will always be the same
+        int x_orig;
+        int y_orig;
+        Grid map;
+        #endregion
+
+        #region Properties
+        public int Num_col_row { get => num_col_row; set => num_col_row = value; }
+        public int X_orig { get => x_orig; set => x_orig = value; }
+        public int Y_orig { get => y_orig; set => y_orig = value; }
+        #endregion
+
+        #region Functions
+        public MainViewModel(Grid grid)
+        {
+            this.map = grid; 
+        }
+
+        public void AddRow_Column(int add)
+        {
+            Num_col_row += add; 
+        }
+
+        public void MoveGridUpDown(int add)
+        {
+            Y_orig += add; 
+        }
+
+        public void MoveGridLeftRight(int add)
+        {
+            X_orig += add; 
+        }
+        #endregion
+
+
+    }
+}
