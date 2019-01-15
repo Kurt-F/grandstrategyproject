05a6749d6eb41fd2e77c5f02e7a31bff211e099f
Thinker222 <es0197@yahoo.com>
2019-01-15 12:10:55 -0500

    Readded MapGui Project

---
+++ GSOCLMapPrototype/MapGUI/Models/MapPiece.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MapGUI.Models
+{
+    public class MapPiece
+    {
+        int x_pos, y_pos, map_node_id;
+        System.Windows.Media.Brush background;
+        public MapPiece()
+        {
+            
+        }
+
+        public MapPiece(int x_pos, int y_pos, int id)
+        {
+            this.x_pos = x_pos;
+            this.y_pos = y_pos;
+            this.map_node_id = id;
+        }
+       
+        public int X_pos { get => x_pos; set => x_pos = value; }
+        public int Y_pos { get => y_pos; set => y_pos = value; }
+        public int Map_node_id { get => map_node_id; set => map_node_id = value; }
+        public Brush Background { get => background; set => background = value; }
+    }
+}
